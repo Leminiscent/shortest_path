d1fb4e6757c2d0bb3222c73c07a852e35a765969
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-01-28 18:59:45 -0500

    Refactor shortest_path function to initialize distances and paths dictionaries

--- shortest_path.py
+++ shortest_path.py
@@ -8,8 +8,9 @@ my_graph = {
 
 def shortest_path(graph, start):
     unvisited = list(graph)
-    distances = {}
-    paths = {key: [] for key in graph}
+    distances = {node: 0 if node == start else float("inf") for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
     print(f"Unvisited: {unvisited}\nDistances: {distances}")
 
 
