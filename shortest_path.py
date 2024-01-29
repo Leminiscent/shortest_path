a9eea9f7acdf1bdd189bf3224998014fd95c408b
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-01-28 19:27:21 -0500

    Fix shortest path algorithm to update paths correctly

--- shortest_path.py
+++ shortest_path.py
@@ -16,6 +16,10 @@ def shortest_path(graph, start):
         for node, distance in graph[current]:
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
+                if paths[node][-1] == node:
+                    paths[node] = paths[current]
+                else:
+                    paths[node].extend(paths[current])
     print(f"Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}")
 
 
